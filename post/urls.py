9d769f758b8f39b8541077e65e64f1a2a08cf0eb
Mexriddin <you@gmail.com>
2020-09-29 08:43:53 +0500

    Add Post model

--- post/urls.py
+++ post/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-    path('', include('post.urls')),
-    path('news/', include('news.urls')),
+    path('',views.index, name="index"),
+    path('contact/', views.contact, name="contact")
 ]
